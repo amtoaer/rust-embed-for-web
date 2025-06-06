58440a9b94043abe8b27a15e1b9b08234542ef01
amtoaer <amtoaer@gmail.com>
2025-06-06 21:57:49 +0800

    ignore compression threshold

--- utils/src/file/common.rs
+++ utils/src/file/common.rs
@@ -24,7 +24,7 @@ use enum_dispatch::enum_dispatch;
 use crate::{DynamicFile, EmbeddedFile};
 
 #[enum_dispatch(EmbedableFile)]
-enum EmbeddedFileImpl {
+pub enum EmbeddedFileImpl {
     EmbeddedFile,
     DynamicFile,
 }
--- utils/src/file/mod.rs
+++ utils/src/file/mod.rs
@@ -2,6 +2,6 @@ mod common;
 mod dynamic;
 mod embed;
 
-pub use common::EmbedableFile;
+pub use common::{EmbedableFile, EmbeddedFileImpl};
 pub use dynamic::DynamicFile;
 pub use embed::EmbeddedFile;
